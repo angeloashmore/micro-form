7c1ca4b24baee841f31915ef0876f5ea1368db7d
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2018-07-28 16:06:25 -1000

    Update form name to form id

--- packages/micro-form/index.js
+++ packages/micro-form/index.js
@@ -3,26 +3,50 @@ const parseBody = require('urlencoded-body-parser')
 const R = require('ramda')
 
 const LIST_SPLIT_CHAR = ','
-const DEFAULT_ALLOW_METHODS = 'POST'
-const DEFAULT_FIELDS_WHITELIST = ''
 
 const options = {
+  /***
+   * process.env.MICRO_FORM_ID
+   *
+   * Identifier for the form.
+   *
+   * Default: 'micro-form'.
+   */
+  id: R.pathOr('micro-form', ['MICRO_FORM_ID'], process.env),
+
+  /***
+   * process.env.MICRO_FORM_ALLOW_METHODS
+   *
+   * List of HTTP methods to accept.
+   *
+   * Default: 'POST'.
+   */
   allowMethods: R.pipe(
-    R.pathOr(DEFAULT_ALLOW_METHODS, ['ALLOW_METHODS']),
+    R.pathOr('POST', ['MICRO_FORM_ALLOW_METHODS']),
     R.toUpper,
     R.split(LIST_SPLIT_CHAR)
   )(process.env),
+
+  /***
+   * process.env.MICRO_FORM_FIELDS_WHITELIST
+   *
+   * List of field names to accept.
+   *
+   * Default: ''.
+   */
   fieldsWhitelist: R.pipe(
-    R.pathOr(DEFAULT_FIELDS_WHITELIST, ['FIELDS_WHITELIST']),
+    R.pathOr('', ['MICRO_FORM_FIELDS_WHITELIST']),
     R.split(LIST_SPLIT_CHAR)
   )(process.env),
 }
 
-const handler = async req =>
-  R.pipeP(
+const handler = async req => ({
+  id: options.id,
+  fields: await R.pipeP(
     parseBody,
     R.pick(options.fieldsWhitelist)
-  )(req)
+  )(req),
+})
 
 const cors = microCors({ allowMethods: options.allowMethods })
 module.exports = cors(handler)
